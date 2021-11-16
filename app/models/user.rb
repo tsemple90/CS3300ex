9e2563cb5be8c44f5478e22c1e3c7fa77582b2be
tsemple90 <tsemple@uccs.edu>
2021-11-16 23:34:11 +0000

    devise

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class Student < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-  
-    validates_presence_of :name
-  end
