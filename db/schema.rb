ab9de679a36fe8788bca3ca172bdf52a3941af57
tsemple90 <tsemple@uccs.edu>
2021-12-02 22:53:27 +0000

    checking ssh keys

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 2021_11_17_023250) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
